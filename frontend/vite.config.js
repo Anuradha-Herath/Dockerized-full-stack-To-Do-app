36f661817cc5113056569f6280d41a64c9b917bb
Anuradha Herath <anuradhaherath2001@gmail.com>
2025-09-13 14:44:43 +0530

    Refactor _redirects format and remove unused vercel.json configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,12 +11,5 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: 3000,
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
   }
 })
